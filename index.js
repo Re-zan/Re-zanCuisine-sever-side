dc3ebf20afdd471f0cbc6a6daa44f36f9d09134f
Re-zan <zannat881all@gmail.com>
2023-05-27 02:57:13 +0600

    testimonial data added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000;
 var cors = require("cors");
 app.use(cors());
 const chefData = require("./data/chefData.json");
+const testimonial = require("./data/testimonial.json");
 
 app.get("/", (req, res) => {
   res.send(chefData);
@@ -13,6 +14,10 @@ app.get("/chefrecipes/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const chefSingleData = chefData.find((data) => parseInt(data.id) === id);
   res.send(chefSingleData);
+});
+
+app.get("/testimonial", (req, res) => {
+  res.send(testimonial);
 });
 
 app.listen(port, () => {
